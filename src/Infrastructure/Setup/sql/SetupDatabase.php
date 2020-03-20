d423453dac755d930276fc0f043a468feb7a9294
Adrian Lüthi <adi.l@bluewin.ch>
2020-03-20 15:48:25 +0100

    add \ to gui imports

--- src/Infrastructure/Setup/sql/SetupDatabase.php
+++ src/Infrastructure/Setup/sql/SetupDatabase.php
@@ -3,11 +3,10 @@ declare(strict_types=1);
 
 namespace srag\asq\Infrastructure\Setup\sql;
 
+use srag\asq\Infrastructure\Persistence\SimpleStoredAnswer;
 use srag\asq\Infrastructure\Persistence\EventStore\QuestionEventStoreAr;
 use srag\asq\Infrastructure\Persistence\Projection\QuestionAr;
 use srag\asq\Infrastructure\Persistence\Projection\QuestionListItemAr;
-use srag\asq\Infrastructure\Persistence\SimpleStoredAnswer;
-use srag\asq\UserInterface\Web\PathHelper;
 
 /**
  * Class SetupDatabase
@@ -31,18 +30,9 @@ class SetupDatabase
 
     public function run() : void
     {
-        global $DIC;
-
-        $DIC->database()->modifyTableColumn('copg_pobj_def','component',['length' => '521']);
-        $DIC->database()->insert("copg_pobj_def", array(
-            'parent_type' => array('text', 'asqq'), 'class_name' => array('text', 'AsqPageObject'), 'directory' => array('text', 'src/UserInterface/Web/Page'), 'component' => array('text', PathHelper::getBasePath(__DIR__)  )));
-
         QuestionEventStoreAr::updateDB();
         QuestionListItemAr::updateDB();
         QuestionAr::updateDB();
         SimpleStoredAnswer::updateDB();
-
-
-
     }
 }
